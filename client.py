0a1336a90c0eb5d1f55ef26fc1184ae89305fea3
Zst3el <50278017+Zst3el@users.noreply.github.com>
2021-03-31 13:05:20 +0200

    L3_Handshake

--- client.py
+++ client.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python3
 
 
-input_string = 'Hello'
-print(type(input_string))
-input_bytes_encoded = input_string.encode()
-print(type(input_bytes_encoded))
-print(input_bytes_encoded)
-output_string=input_bytes_encoded.decode()
-print(type(output_string))
-print(output_string)
-
 import socket
 
 SERVER_ADDRESS = '127.0.0.1'
@@ -19,33 +10,34 @@ sock_service = socket.socket()
 
 sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
 
-print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
-while True:
-    try:
-        dati = input("Inserisci i dati da inviare (0 per terminare la connessione): ")
-    except EOFError:
-        print("\nOkay. Exit")
-        break
-    if not dati:
-        print("Non puoi inviare una stringa vuota!")
-        continue
-    if dati == '0':
-        print("Chiudo la connessione con il server!")
-        break
-    
-    dati = dati.encode()
+print("Client connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
 
+protocollo=["SYN", "SYN ACK", "ACK with Data", "ACK for Data"]
+step=0
+dati = str(step)
+
+while True:
+    dati=dati.encode() 
     sock_service.send(dati)
+    print("Inviato:" + str(step) + " - " + protocollo[step])
+    
 
     dati = sock_service.recv(2048)
 
     if not dati:
         print("Server non risponde. Exit")
         break
-    
-    dati = dati.decode()
 
-    print("Ricevuto dal server:")
-    print(dati + '\n')
+    dati = dati.decode()
+    step = int(dati)
+    if dati == '3':
+        print("Ricevuto:" + str(step) + " - " + protocollo[step])
+        print("Termino la connessione con il server.")
+        break
+    else:
+        step = int(dati)
+        print("Ricevuto:" + str(step) + " - " + protocollo[step])
+        step+=1
+        dati= str(step)
 
 sock_service.close()
--- server.py
+++ server.py
@@ -17,28 +17,41 @@ sock_listen.listen(5)
 
 print("Server in ascolto su %s." % str((SERVER_ADDRESS, SERVER_PORT)))
 
+protocollo=["SYN", "SYN + ACK", "ACK + Data", "ACK for Data"]
 
 while True:
     sock_service, addr_client = sock_listen.accept()
     print("\nConnessione ricevuta da " + str(addr_client))
     print("\nAspetto di ricevere i dati ")
-    contConn=0
+    step=0
+    
     while True:
+        
         dati = sock_service.recv(2048)
-        contConn+=1
         if not dati:
             print("Fine dati dal client. Reset")
             break
         
         dati = dati.decode()
-        print("Ricevuto: '%s'" % dati)
-        if dati=='0':
-            print("Chiudo la connessione con " + str(addr_client))
+        step=int(dati)
+        print("Ricevuto:" + str(step) + " - " + protocollo[step])
+        
+        step+=1
+        dati= str(step)
+        if dati > '3':
+            dati = dati.encode()
+            sock_service.send(dati)
+            print("Inviato:" + str(step) + " - " + protocollo[step])
+            print("Termino la connessione con il client.")
             break
-        dati = "Risposta a : " + str(addr_client) + ". Il valore del contatore è : " + str(contConn)
-
-        dati = dati.encode()
-
-        sock_service.send(dati)
+        else:
+            dati = dati.encode()
+            sock_service.send(dati)
+            print("Inviato:" + str(step) + " - " + protocollo[step])
+            step+1
+            dati = str(step)
+            
+            
+        
 
     sock_service.close()
